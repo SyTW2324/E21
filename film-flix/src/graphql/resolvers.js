73de473ea7ab91f9c6dc05b187db96d20a1e6122
Pablo400-glitch <alu0101311135@ull.edu.es>
2023-11-25 12:09:48 +0000

    Add CRUD operations for films, series, seasons, episodes, and users

--- film-flix/src/graphql/resolvers.js
+++ film-flix/src/graphql/resolvers.js
@@ -26,13 +26,73 @@ const resolvers = {
     allEpisodes: async () => {
       const episodes = await EpisodeModel.find({});
       return episodes;
-    },
+    }
   },
   Mutation: {
     addUser: async (root, args) => {
       const user = new UserModel({...args});
       await user.save();
       return user;
+    },
+    addFilm: async (root, args) => {
+      const film = new FilmModel({...args});
+      await film.save();
+      return film;
+    },
+    addSeries: async (root, args) => {
+      const series = new SeriesModel({...args});
+      await series.save();
+      return series;
+    },
+    addSeason: async (root, args) => {
+      const season = new SeasonModel({...args});
+      await season.save();
+      return season;
+    },
+    addEpisode: async (root, args) => {
+      const episode = new EpisodeModel({...args});
+      await episode.save();
+      return episode;
+    },
+    deleteFilm: async (root, args) => {
+      const film = await FilmModel.findOneAndDelete({title: args.title});
+      return film;
+    },
+    deleteSeries: async (root, args) => {
+      const series = await SeriesModel.findOneAndDelete({title: args.title});
+      return series;
+    },
+    deleteSeason: async (root, args) => {
+      const season = await SeasonModel.findOneAndDelete({title: args.num});
+      return season;
+    },
+    deleteEpisode: async (root, args) => {
+      const episode = await EpisodeModel.findOneAndDelete({title: args.title});
+      return episode;
+    },
+    deleteUser: async (root, args) => {
+      const user = await UserModel.findOneAndDelete({username: args.username});
+      return user;
+    },
+    updateFilm: async (root, args) => {
+      const film = await FilmModel.findOneAndUpdate({title: args.title}, {...args});
+      return film;
+    },
+    updateUser: async (root, args) => {
+      const user = await UserModel.findOneAndUpdate({username: args.username}, {...args});
+      return user;
+    },
+    updateSeries: async (root, args) => {
+      const series = await SeriesModel.findOneAndUpdate({title: args.title}, {...args});
+      return series;
+    },
+    updateSeason: async (root, args) => {
+      const season = await SeasonModel.findOneAndUpdate({num: args.num}, {...args});
+      return season;
+    },
+    updateEpisode: async (root, args) => {
+      const episode = await EpisodeModel.findOneAndUpdate({title: args.title}, {...args});
+      return episode;
     }
   }
 };
