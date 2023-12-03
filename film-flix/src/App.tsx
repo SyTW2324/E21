a28d628e56378d8604cd981143b7d78fc271bb0e
Pablo400-glitch <alu0101311135@ull.edu.es>
2023-12-03 20:03:19 +0000

    Update routes and navbar component

--- film-flix/src/App.tsx
+++ film-flix/src/App.tsx
@@ -6,13 +6,14 @@ import Login from './pages/login/Login';
 import Profile from './pages/profile/Profile';
 import Movies from './pages/movies/movies';
 import Series from './pages/series/series';
+import Home from './pages/Home/Home';
 
 function App() {
   return (
     <div className="App h-full bg-white">
       <BrowserRouter>
         <Routes>
-          <Route path="/" element={<h1>Home</h1>} />
+          <Route path="/" element={<Home />} />
           <Route path="/movies" element={<Movies />} />
           <Route path="/series" element={<Series />} />
           <Route path="/profile" element={<Profile />} />
