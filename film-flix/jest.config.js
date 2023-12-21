b377e1802082826a450adc7e865e9cac27361a0c
Pablo400-glitch <alu0101311135@ull.edu.es>
2023-12-21 11:40:08 +0000

    Update Jest configuration and package.json
    This commit updates the Jest configuration in jest.config.js to include additional module file extensions and coverage thresholds. It also adds new scripts to the package.json file for running test coverage and generating coveralls report.

--- film-flix/jest.config.js
+++ film-flix/jest.config.js
@@ -8,9 +8,17 @@ export default {
     },
     testRegex: '(/__tests__/.*|(\\.|/)(test|spec))\\.(jsx?|tsx?)$',
     moduleDirectories: ["node_modules", "src"],
-    moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
+    moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node', 'lcov', 'clover', 'text'],
     "moduleNameMapper": {
         "\\.(css|less|scss|sass)$": "identity-obj-proxy",
         '\\.(jpg|jpeg|png|gif|svg|ico)$': 'identity-obj-proxy'
     },
+    "coverageThreshold": {
+        "global": {
+          "branches": 80,
+          "functions": 80,
+          "lines": 80,
+          "statements": 80
+        }
+      },
 };
