87c33917356d98d334e9f67b8b73d6bff18d6615
Roxana Mihaela Baba <alu0101339887@ull.edu.es>
2023-12-26 13:42:45 +0000

    Refactor profile page styling

--- film-flix/src/pages/profile/Profile.tsx
+++ film-flix/src/pages/profile/Profile.tsx
@@ -55,13 +55,13 @@ export default function Profile() {
   }, [navigate]);
 
   return (
-    <section className="bg-white dark:bg-gray-900">
+    <section className="bg-gray-900">
       {alertShow && <Alert message={error_message} />}
-      <div className="py-8 lg:py-16 px-4 mx-auto max-w-screen-md">
-        <h2 className="mb-4 text-4xl tracking-tight font-extrabold text-center text-sky-700 dark:text-white">
+      <div className="py-8 lg:py-16 px-4 mx-auto max-w-screen-md flex flex-col w-full h-screen">
+        <h2 className="mb-4 text-4xl tracking-tight font-extrabold text-center text-white">
           {data.username}
         </h2>
-        <p className="mb-8 lg:mb-16 font-light text-center text-sky-900 dark:text-gray-400 sm:text-xl">
+        <p className="mb-8 lg:mb-16 font-light text-center text-sky-200 sm:text-xl">
           ¡Bienvenido/a a tu perfil personal! Aquí podrás echar un vistazo a tus
           datos y modificarlos si lo deseas
         </p>
@@ -69,33 +69,33 @@ export default function Profile() {
           <div>
             <label
               htmlFor="id"
-              className="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300"
+              className="block mb-2 text-sm font-medium text-gray-300"
             >
               Your ID
             </label>
-            <div className="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg w-full p-2.5">
+            <div className="shadow-sm bg-gray-800 border border-gray-300 text-gray-300 text-sm rounded-lg w-full p-2.5">
               {data._id}
             </div>
           </div>
           <div>
             <label
               htmlFor="email"
-              className="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300"
+              className="block mb-2 text-sm font-medium text-gray-300"
             >
               Your email
             </label>
-            <div className="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg w-full p-2.5">
+            <div className="shadow-sm bg-gray-800 border border-gray-300 text-gray-300 text-sm rounded-lg w-full p-2.5">
               {data.email}
             </div>
           </div>
           <div>
             <label
               htmlFor="subject"
-              className="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300"
+              className="block mb-2 text-sm font-medium text-gray-300"
             >
               Favorites Movies
             </label>
-            <div className="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg w-full p-2.5">
+            <div className="shadow-sm bg-gray-800 border border-gray-300 text-gray-300 text-sm rounded-lg w-full p-2.5">
               {data.favoriteMovies.map((movie: any, index: any) => (
                 <div key={index}>{movie.title}</div>
               ))}
@@ -104,11 +104,11 @@ export default function Profile() {
           <div>
             <label
               htmlFor="subject"
-              className="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300"
+              className="block mb-2 text-sm font-medium text-gray-300"
             >
               Favorites Series
             </label>
-            <div className="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg w-full p-2.5">
+            <div className="shadow-sm bg-gray-800 border border-gray-300 text-gray-300 text-sm rounded-lg w-full p-2.5">
               {data.favoriteSeries.map((movie: any, index: any) => (
                 <div key={index}>{movie.title}</div>
               ))}
