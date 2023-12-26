7aea81438238bab149a2ad6c3f19525947abc875
Samuel <alu01013595262ull.edu.es>
2023-12-26 15:27:05 +0000

    Implemented the insertion of new comments to the comment database

--- film-flix/src/pages/contentInfo/contentInfo.tsx
+++ film-flix/src/pages/contentInfo/contentInfo.tsx
@@ -1,6 +1,6 @@
 import Footer from "../../components/footer"
 import Navbar from "../../components/navbar"
-import { useParams } from "react-router-dom"
+import { useParams, useNavigate } from "react-router-dom"
 
 import React from "react"
 
@@ -57,8 +57,28 @@ async function getContentInfo(id: string, type: "movies" | "series", onErr: (err
   }
 }
 
+async function getComments() : Promise<void> {
+  try {
+    const response = await fetch(`http://localhost:3001/comments`, {
+      method: "GET"
+    });
+    if (!response.ok) {
+      throw new Error(`Error en la solicitud: ${response.statusText}`);
+    }
+    const data = await response.json();
+    console.log(data);
+  } catch (error: any) {
+    console.log(error.message);
+  }
+}
+
 export default function ContentInfo({ type }: { type: "movies" | "series"}) {
   const [content, setContent] = React.useState<Movies | Series>();
+  const [comments, getComment] = React.useState<any>([]);
+  const [text, setText] = React.useState("");
+  const [userName, setUserName] = React.useState("");
+
+  const navigate = useNavigate();
 
   // Extraer el id de la url
   const { id } = useParams() as { id: string };
@@ -67,6 +87,53 @@ export default function ContentInfo({ type }: { type: "movies" | "series"}) {
     getContentInfo(id, type, (error) => {
     }).then((data) => setContent(data));
   }, [type, id]);
+
+  React.useEffect(() => {
+   getComments().then((data) => getComment(data));
+  }, []);
+
+  const handleSubmit = async (e: any) => {
+    e.preventDefault();
+    try {
+      const token = localStorage.getItem("token");
+      // LANZAR UN MENSAJE DE ERROR EN ESTE CASO
+      if (!token) {
+        navigate("/login");
+      }
+
+      const responseUser = await fetch("http://localhost:3001/user", {
+        method: "GET",
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: token ?? "",
+        },
+      });
+
+      if (!responseUser.ok) {
+        throw new Error(`Error en la solicitud: ${responseUser.statusText}`);
+      }
+
+      // Obtención del nombre de usuario a partir del token
+      const dataUser = await responseUser.json();
+      setUserName(dataUser.username);
+
+      const response = await fetch("http://localhost:3001/comments", {
+        method: "PUT",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          text,
+          userName,
+        }),
+      });
+
+      console.log(response);
+      navigate("/");
+    } catch (error: any) {
+      console.log(error.message);
+    }
+  };
 
   return (
     <>
@@ -178,16 +245,17 @@ export default function ContentInfo({ type }: { type: "movies" | "series"}) {
           <div className="max-w-2xl mx-auto px-4">
             <div className="flex justify-between items-center mb-6">
               <h2 className="text-lg lg:text-2xl font-bold text-white">
-                Discussion (20)
+                Discussion
               </h2>
             </div>
-            <form className="mb-6">
+            <form className="mb-6" onSubmit={handleSubmit}>
               <div className="py-2 px-4 mb-4 rounded-lg rounded-t-lg borde bg-gray-800 border-gray-700">
                 <label className="sr-only">Your comment</label>
                 <textarea
                   id="comment"
                   className="px-0 w-full text-sm text-white border-0 focus:ring-0 focus:outline-none placeholder-gray-400 bg-gray-800"
                   placeholder="Write a comment..."
+                  onChange={(e) => setText(e.target.value)}
                   required
                 ></textarea>
               </div>
@@ -202,73 +270,17 @@ export default function ContentInfo({ type }: { type: "movies" | "series"}) {
               <footer className="flex justify-between items-center mb-2">
                 <div className="flex items-center">
                   <p className="inline-flex items-center mr-3 text-sm text-white font-semibold">
-                    <img
-                      className="mr-2 w-6 h-6 rounded-full"
-                      src="https://flowbite.com/docs/images/people/profile-picture-2.jpg"
-                      alt="Michael Gough"
-                    />
-                    Michael Gough
+                    {/*<img*/}
+                    {/*  className="mr-2 w-6 h-6 rounded-full"*/}
+                    {/*  src="https://flowbite.com/docs/images/people/profile-picture-2.jpg"*/}
+                    {/*  alt="Michael Gough"*/}
+                    {/*/>*/}
+                    {/*{comments[0]?.userName}*/}
                   </p>
                 </div>
               </footer>
               <p className="text-gray-400">
-                The movie is one of the best I've seen, and I loved it. It's a
-                compelling story with depth, not related to religion. Despite
-                some people not finding it appealing, I highly recommend it. The
-                film excels in its incredible story and serious tone, with a
-                notable aspect being the high level of romance. It's not for
-                everyone, especially not for pessimists. Put aside the science
-                fiction element, approach it with maturity, and try to
-                understand the movie.
-              </p>
-              <div className="flex items-center mt-4 space-x-4"></div>
-            </article>
-            <article className="p-6 text-base rounded-lg bg-gray-900">
-              <footer className="flex justify-between items-center mb-2">
-                <div className="flex items-center">
-                  <p className="inline-flex items-center mr-3 text-sm text-white font-semibold">
-                    <img
-                      className="mr-2 w-6 h-6 rounded-full"
-                      src="https://flowbite.com/docs/images/people/profile-picture-3.jpg"
-                      alt="Bonnie Green"
-                    />
-                    Bonnie Green
-                  </p>
-                </div>
-              </footer>
-              <p className="text-gray-400">
-                The plot, special effects, and cinematography are the highlights
-                of the movie. Just the premise of delving into dreams, something
-                scientifically understood very little, allows for a
-                well-speculated theory like this. The ending is quite epic, and
-                the way they construct it, leaving the doubt about the truth of
-                everything we just saw, is spectacular. Regarding the
-                performances, I think they fell a bit short, especially
-                considering DiCaprio's potential. The plot also gets tangled,
-                making it one of those movies that demands your full attention;
-                otherwise, you'll lose all sense.
-              </p>
-              <div className="flex items-center mt-4 space-x-4"></div>
-            </article>
-            <article className="p-6 text-base rounded-lg bg-gray-900">
-              <footer className="flex justify-between items-center mb-2">
-                <div className="flex items-center">
-                  <p className="inline-flex items-center mr-3 text-sm text-white font-semibold">
-                    <img
-                      className="mr-2 w-6 h-6 rounded-full"
-                      src="https://flowbite.com/docs/images/people/profile-picture-4.jpg"
-                      alt="Helene Engels"
-                    />
-                    Helene Engels
-                  </p>
-                </div>
-              </footer>
-              <p className="text-gray-400">
-                This movie is undoubtedly an explosion of elements that leave
-                you in awe, and you have to watch it two or more times to
-                understand it from start to finish. It features an incredible
-                cast and is directed by a spectacular and imaginative director.
-                One of the best movies I will ever see in my life.
+                {/*{comments[0]?.text}*/}
               </p>
               <div className="flex items-center mt-4 space-x-4"></div>
             </article>
