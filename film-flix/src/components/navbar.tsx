20fc3a3e399f5d55309ca828408d7d71624a2d34
Samuel <alu01013595262ull.edu.es>
2023-12-27 16:47:01 +0000

    Implemented the botton change on the navbar component

--- film-flix/src/components/navbar.tsx
+++ film-flix/src/components/navbar.tsx
@@ -6,6 +6,14 @@ export default function Navbar() {
   const navigate = useNavigate();
   const location = useLocation();
 
+  const isLoggedIn = () : boolean => {
+    if (localStorage.getItem("token")) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+
   return (
     <nav className="border-gray-200 bg-gray-900">
       <div className="max-w-screen-2xl flex flex-wrap items-center justify-between mx-auto p-4">
@@ -16,17 +24,43 @@ export default function Navbar() {
           </span>
         </a>
         <div className="flex order-2 space-x-1 rtl:space-x-reverse">
-          <button
-            type="button"
-            onClick={() => {
-              navigate("/login");
-            }}
-            className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none 
-                                focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 text-center dark:bg-blue-600 
+          {isLoggedIn() ? (
+            <button
+              type="button"
+              onClick={() => {
+                localStorage.removeItem("token");
+                navigate("/");
+              }}
+              className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none
+                                focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 text-center dark:bg-blue-600
                                 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
-          >
-            Get started
-          </button>
+            >
+              Logout
+            </button>
+          ) : (
+            <button
+              type="button"
+              onClick={() => {
+                navigate("/login");
+              }}
+              className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none
+                                focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 text-center dark:bg-blue-600
+                                dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+            >
+              Login
+            </button>
+          )}
+          {/*<button*/}
+          {/*  type="button"*/}
+          {/*  onClick={() => {*/}
+          {/*    navigate("/login");*/}
+          {/*  }}*/}
+          {/*  className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none */}
+          {/*                      focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 text-center dark:bg-blue-600 */}
+          {/*                      dark:hover:bg-blue-700 dark:focus:ring-blue-800"*/}
+          {/*>*/}
+          {/*  Get started*/}
+          {/*</button>*/}
           <button
             data-collapse-toggle="navbar-cta"
             type="button"
