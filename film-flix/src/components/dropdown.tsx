c9dd762bfe09ea78df6af85918e60b0b7c06f81c
Roxana Mihaela Baba <alu0101339887@ull.edu.es>
2023-12-29 00:19:55 +0000

    Fix sign out button in dropdown component

--- film-flix/src/components/dropdown.tsx
+++ film-flix/src/components/dropdown.tsx
@@ -92,7 +92,7 @@ const DropdownComponent = () => {
             </li>
           </ul>
           <div className="py-2">
-            <a
+            <div 
               onClick={() => {
                 localStorage.removeItem("token");
                 navigate("/login");
@@ -100,7 +100,7 @@ const DropdownComponent = () => {
               className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white"
             >
               Sign out
-            </a>
+            </div>
           </div>
         </div>
       )}
