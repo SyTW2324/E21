08b1c95d1655e96817aaf15734d13a9c30ca70a9
Roxana Mihaela Baba <alu0101339887@ull.edu.es>
2024-01-02 17:55:52 +0000

    Fix useEffect dependency warning

--- film-flix/src/pages/login/Login.tsx
+++ film-flix/src/pages/login/Login.tsx
@@ -20,6 +20,7 @@ export default function Login() {
       error_message = errorMessage;
       setShowAlert(true);
     }
+  // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
   const handleSubmit = async (e: any) => {
