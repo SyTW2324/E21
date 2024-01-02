1340badf9d18366e933766c999205c7ed4436031
Roxana Mihaela Baba <alu0101339887@ull.edu.es>
2024-01-02 20:17:50 +0000

    Fix padding in movie and series list

--- film-flix/src/pages/profile/Profile.tsx
+++ film-flix/src/pages/profile/Profile.tsx
@@ -98,7 +98,7 @@ export default function Profile() {
               {
               data.favoriteMovies.map((movie: any, index: any) => (
                 <Link to={`/movies/${movie._id}`} key={movie._id}>
-                  <div className="pt-2" key={index}>{index + 1}. {movie.title}</div>
+                  <div className="pt-2 pb-2" key={index}>{index + 1}. {movie.title}</div>
                 </Link>
               ))
               }
@@ -114,7 +114,7 @@ export default function Profile() {
             <div className="shadow-sm bg-gray-700 border border-gray-300 text-gray-200 text-sm rounded-lg w-full p-2.5">
               {data.favoriteSeries.map((serie: any, index: any) => (
                 <Link to={`/series/${serie._id}`} key={serie._id}>
-                  <div className="pt-2" key={index}>{index + 1}. {serie.title}</div>
+                  <div className="pt-2 pb-2" key={index}>{index + 1}. {serie.title}</div>
                 </Link>
               ))}
             </div>
