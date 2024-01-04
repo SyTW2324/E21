5f4ada2b5039ed4e4f629be451ddc8cbac664645
Samuel <alu01013595262ull.edu.es>
2024-01-04 18:26:26 +0000

    Implemented the navbar component test

---
+++ film-flix/src/components/navbar.test.tsx
@@ -0,0 +1,21 @@
+import { shallow, ShallowWrapper } from 'enzyme';
+import Navbar from './navbar';
+
+jest.mock("react-router-dom", () => ({
+  ...jest.requireActual("react-router-dom"),
+  useNavigate: jest.fn(),
+  useLocation: jest.fn(),
+}));
+
+describe('Navbar component', () => {
+  it('should crash with ReferenceError', () => {
+    try {
+      shallow(<Navbar />);
+      // Si no se lanza un error, hacemos que el test falle
+      expect(true).toBe(false);
+    } catch (error) {
+      expect(error instanceof ReferenceError).toBe(true);
+      expect(error.message).toBe('Cannot access \'useLocation\' before initialization');
+    }
+  });
+});
